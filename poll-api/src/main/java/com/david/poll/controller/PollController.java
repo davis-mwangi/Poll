a18235f2411b8176b78e48796ee647556dbe8fd7
davis-mwangi <daudimwash@gmail.com>
2019-01-22 10:34:12 +0300

    Add react client app

---
+++ poll-api/src/main/java/com/david/poll/controller/PollController.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.david.poll.controller;
+
+import com.david.poll.model.Poll;
+import com.david.poll.payload.ApiResponse;
+import com.david.poll.payload.PagedResponse;
+import com.david.poll.payload.PollRequest;
+import com.david.poll.payload.PollResponse;
+import com.david.poll.payload.VoteRequest;
+import com.david.poll.repository.PollRepository;
+import com.david.poll.repository.UserRepository;
+import com.david.poll.repository.VoteRepository;
+import com.david.poll.security.CurrentUser;
+import com.david.poll.security.UserPrincipal;
+import com.david.poll.service.PollService;
+import com.david.poll.util.AppConstants;
+import java.net.URI;
+import javax.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+/**
+ *
+ * @author david
+ */
+@RestController
+@RequestMapping("/api/polls")
+public class PollController {
+
+    @Autowired
+    private PollRepository pollRepository;
+
+    @Autowired
+    private VoteRepository voteRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PollService pollService;
+
+    private static final Logger logger = LoggerFactory.getLogger(PollController.class);
+
+    @GetMapping
+    public PagedResponse<PollResponse> getPolls(@CurrentUser UserPrincipal currentUser,
+                                                @RequestParam(value = "page", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER) int page,
+                                                @RequestParam(value = "size", defaultValue = AppConstants.DEFAULT_PAGE_SIZE) int size) {
+        return pollService.getAllPolls(currentUser, page, size);
+    }
+
+    @PostMapping
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<?> createPoll(@Valid @RequestBody PollRequest pollRequest) {
+        Poll poll = pollService.createPoll(pollRequest);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest().path("/{pollId}")
+                .buildAndExpand(poll.getId()).toUri();
+
+        return ResponseEntity.created(location)
+                .body(new ApiResponse(true, "Poll Created Successfully"));
+    }
+
+    @GetMapping("/{pollId}")
+    public PollResponse getPollById(@CurrentUser UserPrincipal currentUser,
+                                    @PathVariable Long pollId) {
+        return pollService.getPollById(pollId, currentUser);
+    }
+
+    @PostMapping("/{pollId}/votes")
+    @PreAuthorize("hasRole('USER')")
+    public PollResponse castVote(@CurrentUser UserPrincipal currentUser,
+                         @PathVariable Long pollId,
+                         @Valid @RequestBody VoteRequest voteRequest) {
+        return pollService.castVoteAndGetUpdatedPoll(pollId, voteRequest, currentUser);
+    }
+}
---
+++ poll-api/src/main/java/com/david/poll/util/ModelMapper.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.david.poll.util;
+
+import com.david.poll.model.Poll;
+import com.david.poll.model.User;
+import com.david.poll.payload.ChoiceResponse;
+import com.david.poll.payload.PollResponse;
+import com.david.poll.payload.UserSummary;
+import java.time.Instant;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ *
+ * @author david
+ */
+public class ModelMapper {
+     public static PollResponse mapPollToPollResponse(Poll poll, Map<Long,
+             Long> choiceVotesMap, User creator, Long userVote) {
+        PollResponse pollResponse = new PollResponse();
+        pollResponse.setId(poll.getId());
+        pollResponse.setQuestion(poll.getQuestion());
+        pollResponse.setCreationDateTime(poll.getCreatedAt());
+        pollResponse.setExpirationDateTime(poll.getExpirationDateTime());
+        Instant now = Instant.now();
+        pollResponse.setIsExpired(poll.getExpirationDateTime().isBefore(now));
+
+        List<ChoiceResponse> choiceResponses = poll.getChoices().stream().map(choice -> {
+            ChoiceResponse choiceResponse = new ChoiceResponse();
+            choiceResponse.setId(choice.getId());
+            choiceResponse.setText(choice.getText());
+
+            if(choiceVotesMap.containsKey(choice.getId())) {
+                choiceResponse.setVoteCount(choiceVotesMap.get(choice.getId()));
+            } else {
+                choiceResponse.setVoteCount(0);
+            }
+            return choiceResponse;
+        }).collect(Collectors.toList());
+
+        pollResponse.setChoices(choiceResponses);
+        UserSummary creatorSummary = new UserSummary(creator.getId(), creator.getUsername(), creator.getName());
+        pollResponse.setCreatedBy(creatorSummary);
+
+        if(userVote != null) {
+            pollResponse.setSelectedChoice(userVote);
+        }
+
+        long totalVotes = pollResponse.getChoices().stream().mapToLong(ChoiceResponse::getVoteCount).sum();
+        pollResponse.setTotalVotes(totalVotes);
+
+        return pollResponse;
+    }
+}
